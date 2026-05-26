d92225e3898aa31187f6b4043bb97e497695d590
Lisa Sauvinet <sauvinetlisa@gmail.com>
2026-05-26 11:33:15 +0200

    feat:add SupplierCard (#3)

--- app/(app)/suppliers.tsx
+++ app/(app)/suppliers.tsx
@@ -1,9 +1,38 @@
-import { Text, View } from "react-native";
+import SupplierCard from "@/components/SupplierCard";
+import { useFavorites } from "@/hooks/useFavorites";
+import { useSuppliers } from "@/hooks/useSuppliers";
+import { useState } from "react";
+import { StyleSheet, ScrollView, Text, View } from "react-native";
 
 export default function Suppliers() {
+  const [search, setSearch] = useState<string>("");
+  const [city, setCity] = useState<string>("");
+  const { suppliers, error, loading } = useSuppliers(search, city);
+  const { favorites, handleFavoriteToggle } = useFavorites();
+
   return (
-    <View>
-      <Text>Suppliers</Text>
-    </View>
+    <ScrollView style={styles.main}>
+      <View>
+        {suppliers.map((supplier) => (
+            <SupplierCard 
+              key={supplier.id}
+              supplier={supplier} 
+              isFavorite={favorites.some(
+                (fav) => fav.targetId === supplier.id,
+              )}
+              onFavoriteToggle={() => handleFavoriteToggle(supplier.id)}
+            />
+        ))}
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  main: {
+    marginTop: 48,
+    marginBottom: 96, 
+    padding: 24,
+  }
+  
+});
---
+++ hooks/useSuppliers.ts
@@ -0,0 +1,29 @@
+import api from "@/api/axiosConfig";
+import { Supplier } from "@/types/supplier";
+import { useEffect, useState } from "react";
+
+export function useSuppliers(search: string, city: string) {
+  const [suppliers, setSuppliers] = useState<Supplier[]>([]);
+  const [error, setError] = useState<null | string>(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function loadSuppliers() {
+      try {
+        setLoading(true);
+        setError(null);
+        const response = await api.get<Supplier[]>("/suppliers");
+        setSuppliers(response.data);
+      } catch {
+        setError(
+          "Impossible de charger les fournisseurs pour le moment. Veuillez réessayer plus tard.",
+        );
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadSuppliers();
+  }, [search, city]);
+
+  return { suppliers, error, loading };
+}
