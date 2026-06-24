5832a9eef7709ed6cb25ff19823ea4138498a5aa
Lisa Sauvinet <sauvinetlisa@gmail.com>
2026-06-24 10:18:59 +0200

    feat: add favorites button on DetailsPage and create a context for favorites (#6)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
 import { AuthProvider } from "@/context/AuthProvider";
+import { FavoritesProvider } from "@/context/FavoritesProvider";
 import { Slot } from "expo-router";
 
 export default function RootLayout() {
   return (
     <AuthProvider>
+      <FavoritesProvider>
         <Slot />
+      </FavoritesProvider>
     </AuthProvider>
   );
 }
